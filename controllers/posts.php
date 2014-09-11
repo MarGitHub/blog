04e9476dd1c3e75484061224bbc49706711a6b81
Margit <margit.pilden@gmail.com>
2014-09-11 13:27:34 +0300

    Add single post viewing functionality

--- controllers/posts.php
+++ controllers/posts.php
@@ -2,17 +2,27 @@
 
 class posts extends Controller{
 
-	function index(){
-		$this->posts = get_all("SELECT * FROM post");
-	}
+    function index(){
+        $this->posts = get_all("SELECT * FROM post");
+        $_tags = get_all ("select * from post_tags natural join tag");
+        foreach ($_tags as $tag) {
+            $this->tags[$tag['post_id']][] = $tag['tag_name'];
+        }
+    }
 
     function index_ajax(){
-		echo "\$_POST:<br>";
+        echo "\$_POST:<br>";
         var_dump($_POST);
     }
 
-	function index_post(){
-		echo "\$_POST:<br>";
-		var_dump($_POST);
-	}
+    function index_post(){
+        echo "\$_POST:<br>";
+        var_dump($_POST);
+    }
+
+    function view(){
+        $post_id = $this->params[0];
+        $this->post = get_first("select * from post natural join user where post_id='$post_id'");
+        $this->tags = get_all ("select * from post_tags natural join tag where post_id='$post_id'");
+    }
 }
