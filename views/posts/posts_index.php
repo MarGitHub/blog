2e8d7ea7286f3f65850f603a2abb4f265577c43b
Margit <margit.pilden@gmail.com>
2014-10-13 10:51:31 +0300

    final commit

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,25 +1,18 @@
 <?foreach( $posts as $post ):?>
-    <?foreach ($tags[$post['post_id']]as $tag):?>
-        <a href="#"><span class="label" style="background-color: #5bc0de"><?=$tag?></span></a>
-    <?endforeach?>
     <div class="span8">
-        <h1><a href="<?= BASE_URL ?>posts/view/<?= $post['post_id'] ?>"><?=$post['post_subject']?></a></h1>
+        <h1><a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"><?=$post['post_subject']?></a></h1>
         <p><?=$post['post_text']?></p>
-        <div>
-            <span class="badge badge-success">Posted 2012-07-02 20:47:04</span><div class="pull-right">
-                <?foreach($tags[$post['post_id']] as $tag):?>
-                    <a href="#"><span class="label" style="background-color: #5bc0de"><?=$tag?></span></a><? endforeach?>
-                <span class="label">alice</span>
-                <span class="label">story</span>
-                <span class="label">blog</span>
-                <span class="label">personal</span>
-            </div>
-        </div>
+        <span class="badge badge-success"><?=$post['post_created']?></span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
     </div>
+
+    <?foreach ($tags[$post['post_id']] as $tag):?>
+    <a href="tags/view/<?=$tag?>"><span class="label" style="background-color:#5bc0de"><?=$tag?></span></a><? endforeach?>
 <?endforeach?>
-<hr>
+</br>
+</br>
+</hr>
 <form method="post">
-    <input name="data[post_subject]" type="text" placeholder="Pealkiri"></input><br><br>
-    <textarea name="data[post_text]" placeholder="Sisu" cols="100" rows="10"></textarea><br><br>
-    <button class="btn btn-default">Postita</button>
+    <input name="data[post_subject]" type="text" placeholder="pealkiri"/> <br><br>
+    <textarea name="data[post_text]" placeholder="tekst" id="" cols="30" rows="10"></textarea> </br><br>
+    <button class="btn">Post</button>
 </form>
--- views/posts/posts_view.php
+++ views/posts/posts_view.php
@@ -6,7 +6,7 @@
     <p><?= $post['post_text'] ?></p>
 
     <div>
-        <span class="badge badge-success">Posted 2012-08-02 20:47:04</span>
+        <span class="badge badge-success">Posted 2014-09-06 20:47:04</span>
 
         <div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span>
             <span class="label">personal</span></div>
