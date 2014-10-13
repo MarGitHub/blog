a7ba91461d0f10a09b64afc897b6935862b52d88
Margit <margit.pilden@gmail.com>
2014-10-13 10:16:50 +0300

    Fixed comments

--- views/posts/posts_view.php
+++ views/posts/posts_view.php
@@ -1,25 +1,28 @@
+<?foreach ($tags[$post['post_id']] as $tag):?>
+    <a href="a href="tags/view/<?=$tag['tag_name']?>"><span class="label" style="background-color: #5bc0de"><?=$tag?></span></a> <? endforeach?>
 <div class="span8">
-    <?foreach ($tags as $tag):?><a href="tags/view/<?=$tag['tag_name']?>"><span class="label label-info"><?=$tag['tag_name']?></span></a><?endforeach?>
-    <h1><?=$post['post_subject']?></h1>
-    <p><?=$post['post_text']?></p>
+    <h1><?= $post['post_subject'] ?></h1>
+
+    <p><?= $post['post_text'] ?></p>
+
     <div>
-        <span class="badge badge-success">Posted 2014-08-02 20:47:04</span><div class="pull-right">
-            <?foreach ($tags as $tag):?>
-                <a href="#"><span class="label label -info">
-                    <?=$tag['tag_name']?></span></a>
-            <?endforeach?>
-            <span class="label">alice</span>
-            <span class="label">story</span>
-            <span class="label">blog</span>
-            <span class="label">personal</span>
-        </div>
+        <span class="badge badge-success">Posted 2012-08-02 20:47:04</span>
+
+        <div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span>
+            <span class="label">personal</span></div>
     </div>
+    <hr>
+</div>
+
+<h2>Comments</h2>
+<div class="list-group">
+
     <?foreach ($comments as $comment): ?>
         <div class="commentBox">
             <ul class="commentList">
                 <li>
                     <div class="commenterImage">
-                        <img src="http://lorempixel.com/50/50/people/6" />
+                        <img src="http://lorempixel.com/50/50/people/3/" />
                     </div>
                     <div class="commentText">
                         <?=$comment['comment_created']?>
@@ -28,14 +31,17 @@
                     </div>
                 </li>
             </ul>
-            <form class="form-inline" method="post" role="form">
-                <div class="form-group">
-                    <input class="form-control" type="text" placeholder="Your comments" name="data[comment_text]"/>
-                </div>
-                <div class="form-group">
-                    <button class="btn btn-default" type="submit">Add</button>
-                </div>
-            </form>
         </div>
+
+
     <?endforeach ?>
+    <form class="form-inline" method="post" role="form">
+        <div class="form-group">
+            <input class="form-control" type="text" placeholder="Your comments" name="data[comment_text]"/>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-default" type="submit">Add</button>
+        </div>
+    </form>
+
 </div>
